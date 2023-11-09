b85964dda9b1ef43a253f0039c20b6b612dabd99
kittaro <83721094+kittaro@users.noreply.github.com>
2023-11-09 11:53:14 +0300

    rawraawr

---
+++ z13.pas
@@ -0,0 +1,15 @@
+﻿var s: string; i: integer; n: boolean;
+begin
+  write('Введите строку: ');
+  readln(s);
+  n := true;
+  for i := 1 to length(s) do
+  begin
+    if not (s[i] in ['a', 'b', 'c']) then
+      n := false;
+  end;
+  if n then
+    writeln('Сabтрока содержит данные символы')
+  else
+    writeln('Данная строка содержит другие символы');
+end.
